35494b09383eaf27cdd5a2e67e1a6815ff5de8c2
Keshaba Kumar Maharana <keshabakumarmaharana@gmail.com>
2026-07-29 18:03:43 +0530

    Remove auth route

--- apps/app/src/app/api/auth/route.ts
+++
@@ -1,6 +0,0 @@
-import { proxyAuthActionToConvex } from "@convex-dev/auth/nextjs/server";
-import { NextRequest } from "next/server";
-
-export const POST = (request: NextRequest) => {
-  return proxyAuthActionToConvex(request, {});
-};
